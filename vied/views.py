e9fe1e90307668619c12c4b93652ab8dbdd6fcbf
binmamun <bizzmamun@gmail.com>
2020-06-28 10:04:21 +0600

    Just furnished the repo

---
+++ vied/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import ToDoList, Item
+from .forms import CreateNewList
+
+# Create your views here.
+
+
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+    return render(response, 'veid/list.html', {"ls": ls})
+
+
+def home(response):
+    return render(response, "veid/home.html", {})
+
+
+def create(response):
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = ToDoList(name=n)
+            t.save()
+        return HttpResponseRedirect("/%i" % t.id)
+    else:
+        form = CreateNewList()
+    return render(response, "veid/create.html", {"form": form})
