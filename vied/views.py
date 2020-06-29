9f140d5afde14c13473aec690cd4fa920b625969
binmamun <bizzmamun@gmail.com>
2020-06-29 09:20:37 +0600

    added bootstrap and made some styling

--- vied/views.py
+++ vied/views.py
@@ -20,7 +20,7 @@ def index(response, id):
         elif response.POST.get("newItem"):
             txt = response.POST.get("new")
             if len(txt) > 2:
-                ls.item_set.create(text=txt, complete=True)
+                ls.item_set.create(text=txt, complete=False)
             else:
                 print("invalid")
 
